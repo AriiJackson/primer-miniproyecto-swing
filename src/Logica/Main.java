29a9bb16a484571174be94068c02bfa35901f271
Arii Jackson <ariijackson96@gmail.com>
2021-04-15 18:57:21 -0300

    --Primer mini proyecto con Swing

---
+++ src/Logica/Main.java
@@ -0,0 +1,25 @@
+
+package Logica;
+
+import GUI.Agenda;
+
+/**
+ *
+ * @author Ariana Virginia Amaya Natel (ariijackson96@gmail.com)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Creando ventana Agenda
+        Agenda a_electronica = new Agenda();
+        
+        //Haciendo visible Agenda
+        a_electronica.setVisible(true);
+        
+        //Centrando Angenda
+        a_electronica.setLocationRelativeTo(null);
+        
+    }
+    
+}
